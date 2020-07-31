558c7c8d4e52be2760c9409e78535d589af66548
Francis <francispham89@gmail.com>
2020-07-31 14:08:26 -0700

    Installed & Setup React3Fiber & ThreeJS & Implemented Orbit Controls

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Canvas, useThree, extend } from 'react-three-fiber';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 import './App.css';
+
+extend({ OrbitControls });
+
+function Cube(props) {
+  return (
+    <mesh {...props}>
+      <boxBufferGeometry attach="geometry" args={[2, 2, 1]} /> {/* args = [Width, Height, Depth]  */}
+      <meshStandardMaterial attach="material" color="pink" />
+    </mesh>
+  );
+};
+
+function Scene() {
+  const { 
+    camera,
+    gl: { domElement }
+  } = useThree();
+
+  return (
+    <>
+      <ambientLight />
+      <pointLight intensity={0.3} position={[-1, 2, 4]} />
+      <Cube rotation={[10, 10, 0]} position={[0, 0, 0]}/>
+      <Cube rotation={[10, 20, 0]} position={[2, 2, 0]}/>
+
+      <orbitControls args={[camera, domElement]} />
+    </>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <Scene />
+    </Canvas>
   );
-}
+};
 
 export default App;
